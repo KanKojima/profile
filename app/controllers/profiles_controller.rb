6693ab601bf4fdb575552d436ed1f480f05ea925
EC2 Default User <ec2-user@ip-172-31-23-28.us-east-2.compute.internal>
2019-02-22 14:44:32 +0000

    add link

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,22 @@
 class ProfilesController < ApplicationController
   def index
   end
+  def portforio_1
+  end
+  def portforio_2
+  end
+  def portforio_3
+  end
+  def portforio_4
+  end
+  def portforio_5
+  end
+  def portforio_6
+  end
+  def not_art
+  end
+  def solution
+  end
+  def everyday
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   root to:'profiles#index'
+  get 'portforio/1',to:'profiles#portforio_1'
+  get 'portforio/2',to:'profiles#portforio_2'
+  get 'portforio/3',to:'profiles#portforio_3'
+  get 'portforio/4',to:'profiles#portforio_4'
+  get 'portforio/5',to:'profiles#portforio_5'
+  get 'portforio/6',to:'profiles#portforio_6'
+  get 'design_is/1',to:'profiles#not_art'
+  get 'design_is/2',to:'profiles#solution'
+  get 'design_is/3',to:'profiles#everyday'
 end
