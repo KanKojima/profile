1d70268cfe5ecd03b32b3ad2900da7c2700bbe84
EC2 Default User <ec2-user@ip-172-31-23-28.us-east-2.compute.internal>
2019-04-06 06:21:17 +0000

    third commit

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,58 @@
+// $(function(){
+//     const target = document.getElementsByClassName('target');
+//     // const target = document.getElementById('target_1');
+//     target.addEventListener('mouseenter',function(){
+//      target.style.background ='rgba(0, 0, 0, 1)';
+//     },false);
+//     target.addEventListener('mouseleave',function(){
+//         target.style.background='rgba(179, 179, 179, 1)';
+//     });
+//     const target_1 = document.getElementById('target_1');
+//     target_1.addEventListener('mouseenter',function(){
+//      target_1.style.background='rgba(179, 179, 179, 1)';
+//     //  target_1.textContent = 'こんにちは';
+
+//     },false);
+//     target_1.addEventListener('mouseleave',function(){
+//         target_1.style.background='rgba(26, 25, 23, 1)';
+//     });
+// });
+// $(function(){
+//     const target = document.getElementsByClassName('port_forio_area_1_1');
+//     target.addEventListener('mouseenter',function(){
+//         target.style.background = 'rgba(0, 0, 0, 1)';
+//     },false);
+//     target.addEventListener('mouseleave',function(){
+//         target.style.background = 'rgba(26, 25, 23, 1)';
+//   });
+// });
+
+// $(function(){
+//     $('.target').hover( () => {
+ 
+// 　　$('.target').css(`background`,`rgba(0, 0, 0, 1)`);
+ 
+//  }, function() {
+ 
+// 　　$('.target').css(`background`,`rgba(26, 25, 23, 1)`);
+ 
+//   });
+// });
+
+// $(function(){
+//     const target = document.getElementById('target');
+//     // const target = document.getElementById('target_1');
+//     target.addEventListener('mouseenter',function(){
+//      target.style.background='rgba(0, 0, 0, 1)';
+//     },false);
+//     target.addEventListener('mouseleave',function(){
+//         target.style.background='rgba(26, 25, 23, 1)';
+//     });
+//     const target_1 = document.getElementById('target_1_2');
+//     target_1.addEventListener('mouseenter',function(){
+//      target_1.style.background='rgba(26, 25, 23, 1)';
+//     },false);
+//     target_1.addEventListener('mouseleave',function(){
+//         target_1.style.background='rgba(0, 0, 0, 1)';
+//     });
+// });
