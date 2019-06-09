5f56d6ce1f8fec0cda5d452a6776c55b3d317fd0
EC2 Default User <ec2-user@ip-172-31-23-28.us-east-2.compute.internal>
2019-06-09 01:03:06 +0000

    second

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,62 +1,3 @@
-// $(function(){
-//     const target = document.getElementsByClassName('target');
-//     // const target = document.getElementById('target_1');
-//     target.addEventListener('mouseenter',function(){
-//      target.style.background ='rgba(0, 0, 0, 1)';
-//     },false);
-//     target.addEventListener('mouseleave',function(){
-//         target.style.background='rgba(179, 179, 179, 1)';
-//     });
-//     const target_1 = document.getElementById('target_1');
-//     target_1.addEventListener('mouseenter',function(){
-//      target_1.style.background='rgba(179, 179, 179, 1)';
-//     //  target_1.textContent = 'こんにちは';
-
-//     },false);
-//     target_1.addEventListener('mouseleave',function(){
-//         target_1.style.background='rgba(26, 25, 23, 1)';
-//     });
-// });
-// $(function(){
-//     const target = document.getElementsByClassName('port_forio_area_1_1');
-//     target.addEventListener('mouseenter',function(){
-//         target.style.background = 'rgba(0, 0, 0, 1)';
-//     },false);
-//     target.addEventListener('mouseleave',function(){
-//         target.style.background = 'rgba(26, 25, 23, 1)';
-//   });
-// });
-
-// $(function(){
-//     $('.target').hover( () => {
- 
-// 　　$('.target').css(`background`,`rgba(0, 0, 0, 1)`);
- 
-//  }, function() {
- 
-// 　　$('.target').css(`background`,`rgba(26, 25, 23, 1)`);
- 
-//   });
-// });
-
-// $(function(){
-//     const target = document.getElementById('target');
-//     // const target = document.getElementById('target_1');
-//     target.addEventListener('mouseenter',function(){
-//      target.style.background='rgba(0, 0, 0, 1)';
-//     },false);
-//     target.addEventListener('mouseleave',function(){
-//         target.style.background='rgba(26, 25, 23, 1)';
-//     });
-//     const target_1 = document.getElementById('target_1_2');
-//     target_1.addEventListener('mouseenter',function(){
-//      target_1.style.background='rgba(26, 25, 23, 1)';
-//     },false);
-//     target_1.addEventListener('mouseleave',function(){
-//         target_1.style.background='rgba(0, 0, 0, 1)';
-//     });
-// });
-
 $(function(){
     const name = document.getElementById('my_name');
     const name_Lists = [
@@ -68,7 +9,7 @@ $(function(){
     let rnd = Math.floor(Math.random()* name_Lists.length);   
     name.textContent = name_Lists[rnd];
     
-})
+});
 
 $(function(){
     const dis = document.getElementById('title_dis')
@@ -92,10 +33,10 @@ $(function(){
         ];
         let rnd = Math.floor(Math.random()* footer.length);
         title.textContent = footer[rnd];
-})
+});
 
 $(function(){
-    var particles = Particles.init({
+    const particles = Particles.init({
     selector: '.background',
   sizeVariations: 30,
   color: ['#dcdddd', '#231815', '#888283'],
